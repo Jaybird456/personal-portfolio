d96039ef2cfc2f529624948c162faeb209cfec14
Jayla Nicholas <jsimone1215@gmail.com>
2023-05-16 20:58:01 -0500

    fixed skill section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
   return (
     <div name='home'>
       <div className="p-4 m-auto h-screen flex flex-col justify-between items-center md:flex-row sm:ml-24">
-          <div className='mt-10'>
-            <p className='text-light-white text-xl sm:text-4xl my-2 py-3'>Hi, I'm </p>
-            <h1 className="text-6xl my-2 sm:text-7xl">Jayla Nicholas</h1>
+          <div className='md:mt-10'>
+            <p className='text-light-white  sm:text-4xl my-2 py-3'>Hi, I'm </p>
+            <h1 className="text-5xl my-2 md:text-7xl">Jayla Nicholas</h1>
             <p className='text-light-white my-2 mb-6 py-1 text-xl sm:text-2xl'>Front-end Developer</p>
             <div className='border-solid border-2 w-2/6 md:w-3/6 lg:w-2/6 border-red-600 rounded-full text-center text-light-white 
               py-2 px-3  text-md sm:text-lg hover:border-light-white cursor-pointer'>
@@ -20,7 +20,7 @@ const Home = () => {
             </div>
           </div>
       
-        <div className="m-auto text-md sm:text-lg w-full sm:w-5/6 sm:mx-5 lg:mx-24">
+        <div className="m-auto text-md w-full sm:w-5/6 sm:mx-5 lg:mx-24">
           <p className="text-light-white indent-4">
             I'm a front-end developer with a passion for creating, building, 
             and problem-solving. My journey in web development began in high 
@@ -32,8 +32,6 @@ const Home = () => {
             React, Python, C++, and Chakra UI. I love the challenge that coding presents,
             as every website and problem is unique, and it constantly pushes me to think differently.
           Coding also satisfies my inner builder, as I love the process of putting things together.
-            Combining my love for computers with coding has been the perfect fit for me, and it's why
-            I decided to pursue a career in programming. 
           </p>
         </div>
     </div>
